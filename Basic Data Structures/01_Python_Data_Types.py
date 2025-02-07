b77708d3692db6215ad9cd428a267390c078997f
abhinavdeodhar254 <abhinavajayd@gmail.com>
2025-02-08 01:00:00 +0530

    Commit 02- Adding basic of python

---
+++ Basic Data Structures/01_Python_Data_Types.py
@@ -0,0 +1,28 @@
+"Data types in python"
+"1. Integer"
+"2. floating point number"
+"3. String"
+"""4. List: this is a data structure. [10, "Hello", 203.04]"""
+"""5. Dictionary: Key value pair """
+"""6. Sets -> undordered distinct elements"""
+"""7. tuples -> immutable  and ordered data structure"""
+"""8. boolean-> true and false"""
+
+
+"We can use the python as calculator directly"
+
+2+2     #output 4
+2-2     #output 0
+2*2     #output 4
+2%2     #output 0
+
+#modulus gives us the remainder.
+#divison gives us the quotient.
+
+
+#we can also perform power operations
+
+2**4 #this is nothing but 2^4 = 32
+
+
+
